d606567e7357d18f11563a3cb782c2f533a4d57f
CDN18 <cardinal@codeword.info>
2024-10-24 05:39:56 +0800

    fix duplication handlement

--- src/function.php
+++ src/function.php
@@ -452,12 +452,18 @@ function importBlocks($type, $file_path, $club = null) {
     
     try {
         $table = ($type === 'user') ? 'users_blocks' : 'instances_blocks';
-        $pdo = $db->prepare("INSERT INTO $table (club_id, target, created_at) 
-                             VALUES (:club_id, :target, :created_at) 
-                             ON DUPLICATE KEY UPDATE created_at = LEAST(created_at, VALUES(created_at))");
+        $checkStmt = $db->prepare("SELECT id FROM $table WHERE club_id IS :club_id AND target = :target");
+        $insertStmt = $db->prepare("INSERT INTO $table (club_id, target, created_at) 
+                                    VALUES (:club_id, :target, :created_at)");
         $targets = file($file_path, FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
         foreach ($targets as $target) {
-            $pdo->execute([':club_id' => $club_id, ':target' => $target, ':created_at' => time()]);
+            $checkStmt->execute([':club_id' => $club_id, ':target' => $target]);
+            if ($checkStmt->fetch()) {
+                echo "Warning: Skipping existing rule: $target\n";
+                continue;
+            }
+            
+            $insertStmt->execute([':club_id' => $club_id, ':target' => $target, ':created_at' => time()]);
         }
         
         $db->commit();
